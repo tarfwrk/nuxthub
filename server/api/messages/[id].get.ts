eef5a9f6e301fe90673c86414e274cbb9fd1674b
tarfwrk <149292712+tarfwrk@users.noreply.github.com>
2024-05-24 08:52:04 +0000

    get id

--- server/api/messages/[id].get.ts
+++ server/api/messages/[id].get.ts
@@ -2,7 +2,7 @@ export default eventHandler(async (event) => {
     const id = getRouterParam(event, 'id')
     const db = hubDatabase()
   
-    const { results } = await db.prepare('SELECT * FROM messages WHERE id = ?', ).bind(id).first()
-  
-    return results
+    const message = await db.prepare('SELECT * FROM messages WHERE id = ?', ).bind(id).first()
+
+    return message
   })
