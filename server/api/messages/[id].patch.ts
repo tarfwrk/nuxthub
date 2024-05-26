d6b84efb036bb3a32e0344e08b2e49eb95646d4a
tarfwrk <tarfwrk@gmail.com>
2024-05-26 06:35:56 +0000

    patch6

--- server/api/messages/[id].patch.ts
+++ server/api/messages/[id].patch.ts
@@ -1,11 +1,14 @@
-export default eventHandler(async (event) => {
-  const db = hubDatabase();
+export default eventHandler(async (event)
+ => {
   const { text } = await readBody(event);
+  const id = getRouterParam(event, 'id'); // Extract the id from the request URL
+  const db = hubDatabase();
 
   const result = await db
-  .prepare('UPDATE messages SET text = ?1 WHERE id = ?')
-  .bind(text, id)
-  .run();
+    .prepare('UPDATE messages SET text = ?1
+ WHERE id = ?')
+    .bind(text, id)
+    .run();
 
   if (result.changes === 0) {
     return { error: 'sendNoContent(event, 204)' };
