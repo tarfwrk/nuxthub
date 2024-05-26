bc127b66ef3df24784a25bbfe6372bffe2621c55
tarfwrk <tarfwrk@gmail.com>
2024-05-26 06:40:30 +0000

    patch8

--- server/api/messages/[id].patch.ts
+++ server/api/messages/[id].patch.ts
@@ -1,12 +1,12 @@
-export default eventHandler(async (event)
- => {
+
+export default eventHandler(async (event) => {
   const { text } = await readBody(event);
   const id = getRouterParam(event, 'id'); // Extract the id from the request URL
   const db = hubDatabase();
 
   const result = await db
-    .prepare('UPDATE messages SET text = ?1
- WHERE id = ?')
+    
+.prepare('UPDATE messages SET text = ?1 WHERE id = ?')
     .bind(text, id)
     .run();
 
